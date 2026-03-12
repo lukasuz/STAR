614e9cd121bb5a1c69c341bebcf191f8070e2886
lukasuz <lukas@uzolas.com>
2026-03-12 11:36:23 +0100

    pass model path directly, so model init is independent of the config file

--- star/pytorch/star.py
+++ star/pytorch/star.py
@@ -33,18 +33,8 @@ from ..config import cfg
 
 
 class STAR(nn.Module):
-    def __init__(self,gender='female',num_betas=10):
+    def __init__(self,path_model,num_betas=10):
         super(STAR, self).__init__()
-
-        if gender not in ['male','female','neutral']:
-            raise RuntimeError('Invalid Gender')
-
-        if gender == 'male':
-            path_model = cfg.path_male_star
-        elif gender == 'female':
-            path_model = cfg.path_female_star
-        else:
-            path_model = cfg.path_neutral_star
 
         if not os.path.exists(path_model):
             raise RuntimeError('Path does not exist %s' % (path_model))
